c537182ca092594b65e2a01433be6ef58455e045
Yadier <151584896+MrYady@users.noreply.github.com>
2024-11-06 04:19:24 -0400

    Add files via upload

--- funcion.js
+++ funcion.js
@@ -1,54 +1,26 @@
-const slider = [...document.querySelectorAll('.imgs')];
-const currentElement = Number(document.querySelector('.slider_show').dataset.id);
-let value;
-value = currentElement 
+const viewslider = [...document.querySelectorAll('.viewslider')];
+const menuItens = [...document.querySelectorAll('.navegacion')];
+console.log(viewslider)
+
 const options={
     root: null,
     rootMargin: '0px',
-    threshold: 0.25
+    threshold: 0.10
 }
 
 const observar = new IntersectionObserver(callback, options);
-const elemt = document.querySelector('.cont-slider');
-
-observar.observe(elemt)
-console.log.toString(observar.value)
+    observar.observe(viewslider[0]);
 
 
 function callback(entries){
-
-setInterval(slider_show, 5000)
-
-}
-
-
-function slider_show(){
-    console.log('Iniciando...')
-     for(value === 0; value == slider.length+1;){
-        value = value === 0 ? slider.length : 0;
-        console.log("listo")
-        value++
-    }
-    slider[currentElement-1].classList.toggle('slider_show');
-}
-
-
-let listElemts = document.querySelectorAll('.list-base');
-
-listElemts.forEach(listElemt =>{
-    listElemt.addEventListener('click', ()=>{
-        
-        let height = 0;
-        let menu = listElemt.nextElementSibling;
-        let men = listElemt.previousElementSibling;
-        console.log(men)
-        
-        if(menu.clientHeight == "0"){
-            height = menu.scrollHeight;
+    entries.forEach(Entry => {
+        if(Entry.isIntersecting == false){
+            console.log('listo')
+            menuItens[0].classList.toggle("viewAfter");
+        }else{
+            menuItens[0].classList.remove("viewAfter");
         }
+    });
 
-        menu.style.height = height+"px";
-    })
-})
-
+}
 
